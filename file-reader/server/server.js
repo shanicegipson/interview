1c8407363640f969686623f430f699394a31984c
Shanice Gipson <shanicegipson13@gmail.com>
2019-10-27 16:41:01 -0500

    updated code to not take mulitple files

--- file-reader/server/server.js
+++ file-reader/server/server.js
@@ -1,17 +1,19 @@
+const fs = require('fs');
+const formidable = require('formidable');
 const express = require('express');
-const bodyParser = require ('body-parser');
-
 const app = express();
 const PORT = 5000;
 
-const fileRouter = require('./routes/readfile.router');
+// const fileRouter = require('./routes/readfile.router');
 
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({ extended:true}));
-app.use(bodyParser.json());
 
 
-app.use('/api/read', fileRouter);
+// var upload_html = fs.readFileSync("upload_file.html");
+// var upload_path = "/Users/shanice/Documents/mysidewalk/codechallenge/interview/file-reader/server/public/";
+
+
+
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
